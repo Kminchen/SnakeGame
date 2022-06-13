4c31cbf117235d0986e0210c4a37cbe7f7b277fb
Kminchen <s1092807@pu.edu.tw>
2022-06-13 16:22:55 +0800

    c1

---
+++ app/src/main/java/com/example/snakegame/MainActivity2.java
@@ -0,0 +1,39 @@
+package com.example.snakegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity2.this,MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
